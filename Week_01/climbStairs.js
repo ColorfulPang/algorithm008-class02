b0d797a69e37c4e3f40db3adae6b9cd351c03078
庞家富 <colorful.pang@1data.info>
2020-04-19 17:09:23 +0800

    [w1] 优化爬楼算法

--- Week_01/climbStairs.js
+++ Week_01/climbStairs.js
@@ -3,17 +3,22 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  if (climbStairs.caches[n]) {
-    return climbStairs.caches[n]
-  }
   if (n === 1) {
     return 1
   } else if (n === 2) {
     return 2
   }
 
-  var result = climbStairs(n - 1) + climbStairs(n - 2)
-  climbStairs.caches[n] = result
+  var i = 1
+  var j = 2
+  var index = 0
+  var result = i + j
+
+  while(index < n - 3) {
+    i = j
+    j = result
+    result = i + j
+    index++
+  }
   return result
 };
-climbStairs.caches = {}
